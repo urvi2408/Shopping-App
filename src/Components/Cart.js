8ec9a1a14c79ebb10fd7bdf1636f6d4e257e9342
urvi2408 <bhavsarurvi143@gmail.com>
2025-03-20 11:26:41 +0530

    change UI and add header and footer in filter and cart component

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,40 +1,38 @@
-import React,{useState,useEffect}from 'react';
-import axios from 'axios';
-import '../style/App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../style/App.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Cart() {
+  const [product, setProduct] = useState([]);
 
-  const [product ,setProduct]=useState([]);
-  
-  useEffect(()=> {
-    axios.get(`https://fakestoreapi.com/products/5`)
-    .then((Response) => {
-     console.log(Response.data);
-     setProduct(Response.data);
-     console.log(setProduct)
+  useEffect(() => {
+    axios.get(`https://fakestoreapi.com/products/5`).then((Response) => {
+      console.log(Response.data);
+      setProduct(Response.data);
+      console.log(setProduct);
     });
-},[]) 
+  }, []);
 
   return (
-   <div>
-      <div className='details'>
+    <div>
+      <Header />
+      <div className="details">
+        <div className="img2">
+          <img alt="#" className="product_img" src={product.image} />
+        </div>
 
-         <div className='img2'>
-         <img alt="#" className='product_img'src={product.image} />
-         </div>
-      
-         <div className='description'>
-             <h4>{product?.title}</h4>
-             <h6>{`price($) : ${product?.price}`}</h6>
-             <h4>{`category : ${product?.category}`}</h4>
-             <h4>{`Description : ${product?.description}`}</h4>
-         </div>
-
+        <div className="description">
+          <h4>{product?.title}</h4>
+          <h6>{`price($) : ${product?.price}`}</h6>
+          <h4>{`category : ${product?.category}`}</h4>
+          <h4>{`Description : ${product?.description}`}</h4>
+        </div>
       </div>
-   </div>
+      <Footer />
+    </div>
   );
 }
 
 export default Cart;
-
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,43 +1,63 @@
-import React,{useState} from 'react';
-import { useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../style/App.css";
 
 function Login() {
-
-  const[username,setUserName] = useState("");
-  const[password,setPassWord] = useState("");
-  const[data,setData] = useState([]);
+  const [username, setUserName] = useState("");
+  const [password, setPassWord] = useState("");
+  const [data, setData] = useState([]);
   const navigate = useNavigate();
+  const credentials = {
+    username: "eve.holt@reqres.in",
+    password: "cityslicka",
+  };
 
-  const onsubmit =(e) => {
+  const onsubmit = (e) => {
     e.preventDefault();
-    axios.post("https://fakestoreapi.com/auth/login",{username:username ,password:password})
-    .then((response)=>{
-        if(response.data.token){
-            navigate('/Home');
-        }
-        else{
-            <p>incorrect information</p>
-        }
-        setData(response.data);
-        console.log(data);
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
     })
-    setData([data]);
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.token) {
+          navigate("/Home");
+        } else {
+          <p>incorrect information</p>;
+        }
+        setData(data);
+      })
+      .catch((error) => console.error("Login failed:", error));
+  };
 
   return (
-    <div className='login'>
-        <form onsubmit={onsubmit}>
-            <h2>Login</h2>
-            <input type="text" placeholder='enter your username' value={username} onChange={(e)=>setUserName(e.target.value)}/><br/><br/>
-            <input type="password" placeholder='enter your password' value={password} onChange={(e)=>setPassWord(e.target.value)}/><br/><br/>
-            <button onClick={onsubmit}>Login</button>
-        </form>
+    <div className="login">
+      <form onsubmit={onsubmit}>
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="enter your username"
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="enter your password"
+          value={password}
+          onChange={(e) => setPassWord(e.target.value)}
+        />
+        <br />
+        <br />
+        <button onClick={onsubmit}>Login</button>
+      </form>
 
-        <p>{data.token}</p>
+      <p>{data.token}</p>
     </div>
-  )
+  );
 }
 
 export default Login;
